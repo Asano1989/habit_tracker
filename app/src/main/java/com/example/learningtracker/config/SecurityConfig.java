ab53b69f222d4ecc9a960a8c53e0a9430fb1426b
Asano1989 <asano1989.b@gmail.com>
2025-10-28 09:58:27 +0900

    🛂 SpringSecurityの認証範囲設定

---
+++ app/src/main/java/com/example/learningtracker/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.learningtracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login //  フォーム認証を使う
+        .permitAll()) //  フォーム認証画面は認証不要
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers("/css/**").permitAll() // CSSファイルは認証不要
+            .requestMatchers("/todo", "/todo/**").permitAll()
+            .requestMatchers("/").permitAll() //  トップページは認証不要
+            .anyRequest().authenticated() //  他のURLはログイン後アクセス可能
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
