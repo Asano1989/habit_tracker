1b0fe9f8cde59184afbe673bb720b72a4fbfebd1
Asano1989 <asano1989.b@gmail.com>
2025-10-31 11:23:36 +0900

    🔥 コメントアウト部分削除

--- app/src/main/java/com/example/learningtracker/controller/form/UserForm.java
+++ app/src/main/java/com/example/learningtracker/controller/form/UserForm.java
@@ -23,25 +23,4 @@ public class UserForm implements Serializable {
     @Size(min = 1, max = 50, message = "名前は50文字以内で入力してください")
     @Pattern(regexp="[a-zA-Z0-9亜-熙ぁ-んァ-ヶ]+", message="名前はひらがな、カタカナ、漢字、半角英数字で入力してください")
     private String name;
-
-    /* 
-    public String getAccountId() {
-        return accountId;
-    }
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    */
 }
