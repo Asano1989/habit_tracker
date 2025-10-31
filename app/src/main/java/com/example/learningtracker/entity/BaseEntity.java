439731f5759e6bd082d08a4dff2619e0acb4bef6
Asano1989 <asano1989.b@gmail.com>
2025-10-31 14:53:39 +0900

    ➕ DateTimeFormatアノテーションを追加

--- app/src/main/java/com/example/learningtracker/entity/BaseEntity.java
+++ app/src/main/java/com/example/learningtracker/entity/BaseEntity.java
@@ -6,21 +6,24 @@ import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import lombok.Data;
 
 @Data
-@MappedSuperclass                               // このクラスのフィールドを継承先のテーブルにマッピングする
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)  // タイムスタンプの自動更新を有効化
 public abstract class BaseEntity {
 
     @CreatedDate
     @Column(nullable = false, updatable = false) // 作成後は更新されない
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 }
