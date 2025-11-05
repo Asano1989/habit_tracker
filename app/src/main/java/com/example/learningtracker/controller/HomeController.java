0bc1a34b2414dcbb2f3a6954806660d149a68c02
Asano1989 <asano1989.b@gmail.com>
2025-11-05 16:47:53 +0900

    💄 ビューを綺麗に

--- app/src/main/java/com/example/learningtracker/controller/HomeController.java
+++ app/src/main/java/com/example/learningtracker/controller/HomeController.java
@@ -1,22 +1,33 @@
 package com.example.learningtracker.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.learningtracker.config.LoginUserDetails;
+import com.example.learningtracker.service.RecordService;
+import com.example.learningtracker.entity.Record;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 
 @Controller
 public class HomeController {
+    @Autowired
+    private RecordService recordService;
 
     @GetMapping("/")
-    public ModelAndView home(Model model, @ModelAttribute("message") String message, ModelAndView mv) {
+    public ModelAndView home(@AuthenticationPrincipal LoginUserDetails loginUser, @ModelAttribute("message") String message, ModelAndView mv) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String)) {
+            List<Record> recordList = recordService.findAllRecordsByUser(loginUser);
+            mv.addObject("recordList", recordList);
             mv.addObject("message", "ログインしています");
             mv.addObject("loggedin", true);
 
