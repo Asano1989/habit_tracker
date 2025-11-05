3d62fa4897aa66e53c86b10d5b9cbb6a5c63f107
Asano1989 <asano1989.b@gmail.com>
2025-11-05 17:43:15 +0900

    ♻️ 画像とjsファイルへのアクセスを許可

--- app/src/main/java/com/example/learningtracker/config/SecurityConfig.java
+++ app/src/main/java/com/example/learningtracker/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,14 +11,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    private UserDetailsService userDetailsService;
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.formLogin(login -> login //  フォーム認証を使う
         .permitAll()) //  フォーム認証画面は認証不要
         .authorizeHttpRequests(authz -> authz
-            .requestMatchers("/css/**").permitAll()
+            .requestMatchers("/css/**", "/js/**", "/image/**").permitAll()
             .requestMatchers("/todo", "/todo/query").permitAll()
             .requestMatchers("/signup", "/login", "/logout", "/error").permitAll()
             .requestMatchers("/").permitAll()
