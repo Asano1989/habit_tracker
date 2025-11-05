ee47317d6c0f8b0f7fbf37a756658bbfe72266e5
Asano1989 <asano1989.b@gmail.com>
2025-11-05 13:07:04 +0900

    🐛 Subjectに一部の漢字が入力できないのを修正

--- app/src/main/java/com/example/learningtracker/form/LearningSubjectForm.java
+++ app/src/main/java/com/example/learningtracker/form/LearningSubjectForm.java
@@ -14,11 +14,11 @@ public class LearningSubjectForm implements Serializable {
     private Integer userId;
 
 	@Size(max = 255, message = "項目名は255文字以内で入力してください")
-    @Pattern(regexp="[a-zA-Z0-9亜-熙ぁ-んァ-ヶ+&@#/%?=~_|!:,.;]+", message="項目名はひらがな、カタカナ、漢字、半角の英数字と記号で入力してください")
+    @Pattern(regexp="[a-zA-Z0-9一-龠ぁ-んァ-ヶ+&@#/%?=~_|!:,.;]+", message="項目名はひらがな、カタカナ、漢字、半角の英数字と記号で入力してください")
 	private String name;
 
     @Size(max = 255, message = "説明は500文字以内で入力してください")
-    @Pattern(regexp="[a-zA-Z0-9亜-熙ぁ-んァ-ヶ+&@#/%?=~_|!:,.;]+", message="説明はひらがな、カタカナ、漢字、半角の英数字と記号で入力してください")
+    @Pattern(regexp="[a-zA-Z0-9一-龠ぁ-んァ-ヶ+&@#/%?=~_|!:,.;]+", message="説明はひらがな、カタカナ、漢字、半角の英数字と記号で入力してください")
     private String description;
     
     private User user;
