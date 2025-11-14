33b1ba997cbf5daca2b594a77ccd4a643ce61676
Asano1989 <asano1989.b@gmail.com>
2025-11-14 15:47:32 +0900

    💄FullCalendarの導入

--- app/src/main/resources/static/js/calendar.js
+++ app/src/main/resources/static/js/calendar.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [ ],
+    headerToolbar: {
+      start: 'prevYear,nextYear',
+      center: 'title',
+      end: 'dayGridMonth,listMonth,prev,next'
+    },
+    locale: 'ja',
+    businessHours: true,
+    contentHeight: "auto",
     initialView: 'dayGridMonth'
   });
     calendar.render();
