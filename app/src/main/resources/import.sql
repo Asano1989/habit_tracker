285f80e47677a730ff9d21edd6458a35df44a1e6
Asano1989 <asano1989.b@gmail.com>
2025-11-18 15:19:44 +0900

    🗃️ DBの接続設定をデータ初期化の内容を変更

--- app/src/main/resources/import.sql
+++ app/src/main/resources/import.sql
@@ -1,5 +1,3 @@
-INSERT INTO users(id, account_id, password, name, created_at, updated_at) VALUES (1, 'admin', '$2a$10$3Ig0SBonbQXik.IUrxNMh.2By/JmZiQvNhvO/.aFCrjv4Ad3Omp8m', 'admin', NOW(), NOW());
-INSERT INTO users(id, account_id, password, name, created_at, updated_at) VALUES (2, 'asano69a', '$2a$10$m9FxtHPwoZFDyCyNPjJd4OpoBHFavlbhWhB/fDTT5UI3Rvde2ocnK', 'あさの', NOW(), NOW());
 SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));
 SELECT setval('learning_subjects_id_seq', (SELECT MAX(id) FROM learning_subjects));
 SELECT setval('records_id_seq', (SELECT MAX(id) FROM records));
