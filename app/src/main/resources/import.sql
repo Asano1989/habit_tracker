8e4322c1778e99f6771687b62ff0cdfc3e56e1b0
Asano1989 <asano1989.b@gmail.com>
2025-11-21 15:13:00 +0900

    🚀 deploy準備

--- app/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));
-SELECT setval('learning_subjects_id_seq', (SELECT MAX(id) FROM learning_subjects));
-SELECT setval('records_id_seq', (SELECT MAX(id) FROM records));
